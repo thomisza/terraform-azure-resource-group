9c8376be2a2a58c409d98f5aa70983b06c718643
Thomas ZAWISZA <thomas.zawisza@octo.com>
2021-09-01 11:47:03 +0200

    main update

--- example/main.tf
+++ example/main.tf
@@ -11,7 +11,7 @@ locals {
 }
 
 module "azure_ad_sp" {
-  source                    = "git::https://github.com/0xdbe-terraform/terraform-azure-ad-service-principal.git?ref=v2.0.1"
+  source                    = "git::https://github.com/thomisza/tf-az-sp.git?ref=v2.0.1"
   application_full_name     = local.application_full_name
   application_environment   = local.application_environment
   service_principal_purpose = local.service_principal_purpose
